6fc748518bec51eec012af27b13acf3e15775cc7
aatman <aatman.panseriya@gmail.com>
2024-10-26 13:39:35 +0530

    OpenAI Documentation Inegration(Swagger).com/docs

--- accounts/src/main/java/com/micro/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/micro/accounts/controller/AccountsController.java
@@ -2,20 +2,33 @@ package com.micro.accounts.controller;
 
 import com.micro.accounts.constants.AccountConstants;
 import com.micro.accounts.dto.CustomerDto;
+import com.micro.accounts.dto.ErrorResponseDto;
 import com.micro.accounts.dto.ResponseDto;
 import com.micro.accounts.repository.CustomerRepository;
 import com.micro.accounts.service.IAccountsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name = "CRUD REST APIs for Accounts in EazyBank",
+        description = "CRUD REST APIs in EazyBank to CREATE, UPDATE, FETCH AND DELETE account details"
+)
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 @AllArgsConstructor
+@Validated
 public class AccountsController {
 
     private IAccountsService accountsService;
@@ -26,6 +39,25 @@ public class AccountsController {
         return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDto(AccountConstants.STATUS_201, AccountConstants.MESSAGE_201));
     }
 
+
+    @Operation(
+            summary = "Create Account REST API",
+            description = "REST API to create new Customer &  Account inside EazyBank"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "HTTP Status CREATED"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
     @GetMapping("/fetch")
     public ResponseEntity<CustomerDto> fetchAccountDetail(@RequestParam
                                                               @Pattern(regexp = "^[0-9]{10}$", message = "Invalid Account number")
@@ -34,6 +66,29 @@ public class AccountsController {
        return ResponseEntity.status(HttpStatus.OK).body(customerDto);
     }
 
+
+    @Operation(
+            summary = "Update Account Details REST API",
+            description = "REST API to update Customer &  Account details based on a account number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "Expectation Failed"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
     @PutMapping("/update")
     public ResponseEntity<ResponseDto> updateAccount(@Valid @RequestBody CustomerDto customerDto) {
         boolean isUpdated = accountsService.updateAccount(customerDto);
@@ -45,6 +100,28 @@ public class AccountsController {
         }
     }
 
+    @Operation(
+            summary = "Delete Account & Customer Details REST API",
+            description = "REST API to delete Customer &  Account details based on a mobile number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "Expectation Failed"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
     @DeleteMapping("/delete")
     public ResponseEntity<ResponseDto> deleteAccount(@RequestParam
                                                          @Pattern(regexp = "^[0-9]{10}$", message = "Invalid Account number")
--- accounts/src/main/java/com/micro/accounts/dto/CustomerDto.java
+++ accounts/src/main/java/com/micro/accounts/dto/CustomerDto.java
@@ -1,5 +1,6 @@
 package com.micro.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
@@ -7,18 +8,34 @@ import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
+@Schema(
+        name = "Customer",
+        description = "Schema to hold Customer and Account information"
+)
 public class CustomerDto {
 
+    @Schema(
+            description = "Name of the customer", example = "Eazy Bytes"
+    )
     @NotEmpty(message = "Name cannot be empty")
     @Size(min = 5, max = 50, message = "Name should be between 3 and 50 characters")
     private String name;
 
+    @Schema(
+            description = "Email address of the customer", example = "tutor@eazybytes.com"
+    )
     @NotEmpty(message = "Email cannot be empty")
     @Email(message = "Invalid email format")
     private String email;
 
+    @Schema(
+            description = "Mobile Number of the customer", example = "9345432123"
+    )
     @Pattern(regexp = "^[0-9]{10}$", message = "Invalid mobile number")
     private String mobileNumber;
 
+    @Schema(
+            description = "Account details of the Customer"
+    )
     private AccountsDto accountsDto;
 }
